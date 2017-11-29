ccc1c57bb1462a783482e1bf0baaec88e615775e
dragnet-sh <dragnet.sh@gmail.com>
2017-11-29 21:33:00 +0545

    add the init call

--- state.py
+++ state.py
@@ -39,14 +39,52 @@ class Status(db.Entity):
     tracker = Set('Tracker')
 
 class Tracker(db.Entity):
-    udh_id = Required(unicode)
-    udh_payload = Required(unicode)
+    tracker_id = Required(unicode)
+    payload = Required(unicode)
     action = Optional(Action)
     status = Optional(Status)
     last_known_error = Optional(unicode)
 
 
 sql_debug(False)
-db.bind('sqlite', 'tracker.sqlite', create_db=True)
-db.generate_mapping(create_tables=True)
+db.bind('sqlite', 'tracker.sqlite', create_db = True)
+db.generate_mapping(create_tables = True)
+
+
+
+@db_session
+def init_state(process_id, payload, action,nt status):
+	action = Action.get(label = action)
+	status = Status.get(label = status)
+	
+	Tracker(tracker_id = process_id, payload = payload, action = action, status = status)
+
+@db_session
+def init_action():
+	actions = [EAction.FILE_PARSING, EAction.FILE_TRANSFER]
+	
+	for action in actions:
+		Action(label = action)
+
+@db_session
+def init_status():
+	labels = [EStatus.INIT, EStatus.PROGRESS, EStatus.COMPLETED, EStatus.ERROR]
+	
+	for label in labels:
+		Status(label = label)
+
+
+@db_session
+def status_update(process_id, action, satus):
+	tracker = Tracker.select(lambda t: t.tracker_id).order_by(desc(Tracker.id)).first()
+	tracker.action = Action.get(label = action).id
+	tracker.status = Status.get(label = status).id
+
+
+if __name__ == '__main__':
+	
+	init_action()
+	init_status()
+
+	init_state('tracker-001', 'json-payload', EAction.FILE_TRANSFER, EStatus.PROGRESS)
 
